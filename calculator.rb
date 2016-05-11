65f3ab624cfab3eb1dd5dc81dbb7e6c42d0af09f
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-05-11 15:57:38 -0600

    'first calculator commit'

--- calculator.rb
+++ calculator.rb
@@ -1,17 +1,71 @@
+@arr = ['+', '-', '*', '/']
+# @first_number = 0
+# @operator = ''
+# @second_number = 0
 # create / edit a file
 # stage the file - git add
 # make a commit
 # push to a github repo
 
-puts "Welcome to the ruby calculator"
-puts "Enter your first number:"
-# get the first number
-puts "enter your operator"
-# get the operator
-puts "enter the second number"
-# get the second number
+def first_input
+	puts "Welcome to the Ruby Calculator"
+	puts "Enter your first number:"
+	first_number = gets.to_f
+	operator_input(first_number)
+end
 
-# figure out how to do the math on the two numbers
-# output the result to the user
+def operator_input(first)
+	puts "Enter your operator"
+	@arr.length do |num|
+		puts @arr[num]
+	end
+	operator = gets.chomp.to_s
+	array = [first, operator]
+	second_input(array)
+end
 
-# do all the bonuses
+def second_input(array)
+	puts "Enter the second number"
+	second_number = gets.to_f
+	array << second_number
+	calculate(array)
+end
+
+def calculate(array)
+	if array[1].to_s == "+"
+		answer = array[0] + array[2]
+	elsif array[1].to_s == "-"
+		answer = array[0] - array[2]
+	elsif array[1].to_s == "*"
+		answer = array[0] * array[2]
+	elsif array[1].to_s == "/"
+		answer = array[0] / array[2]
+	else
+		puts "Bad Operator"
+	end
+	display_answer(array, answer)
+end
+
+def display_answer(array, answer)
+	puts "#{array[0]} #{array[1]} #{array[2]} = #{answer}"
+end
+
+
+
+first_input
+
+	
+	# put all inputs into an eqaution
+
+
+
+	# figure out how to do the math on the two numbers
+
+
+
+	 
+	# output the result to the user
+
+
+
+	# do all the bonuses
