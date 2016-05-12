e59f54c2c55470f4cf8389ed10bd4a6a69280a2d
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2016-05-12 13:43:44 -0600

    2nd update

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-@arr = ['+', '-', '*', '/']
+@arr = ['+,', '-,', '*,', '/']
 # @first_number = 0
 # @operator = ''
 # @second_number = 0
@@ -10,23 +10,42 @@
 def first_input
 	puts "Welcome to the Ruby Calculator"
 	puts "Enter your first number:"
-	first_number = gets.to_f
+	begin
+	first_number = gets
+	first_number = Integer(first_number)
+rescue
+	puts "Doesn't work, Please enter a number"
+	retry
+end
 	operator_input(first_number)
 end
 
 def operator_input(first)
 	puts "Enter your operator"
-	@arr.length do |num|
-		puts @arr[num]
+	@arr.each do |num|
+		print num
 	end
+	puts ""
+	
 	operator = gets.chomp.to_s
-	array = [first, operator]
-	second_input(array)
+	while operator != "+" && operator != "-" && operator != "*" && operator != "/" 		
+	  puts "Please use +, -, *, /" 
+	  puts "Please enter your operator again."
+	  operator = gets.chomp.to_s
+	end
+		array = [first, operator]
+		second_input(array)
 end
 
 def second_input(array)
 	puts "Enter the second number"
-	second_number = gets.to_f
+	begin	
+		second_number = gets
+		second_number = Integer(second_number)
+rescue
+	puts "Doesn't work, Please enter a number"
+	retry
+	end	
 	array << second_number
 	calculate(array)
 end
@@ -51,21 +70,10 @@ def display_answer(array, answer)
 end
 
 
-
+# first_input
+while true
 first_input
-
-	
-	# put all inputs into an eqaution
-
-
-
-	# figure out how to do the math on the two numbers
-
-
-
-	 
-	# output the result to the user
-
-
-
-	# do all the bonuses
+puts "Do you want to run another calculation (y/n)"
+choice = gets.strip.downcase
+exit (0) if choice == 'n' || choice == 'no'
+end
